5ada9d106f8d4099adad151d64f08d98469ce91e
khalilou88 <khalil.lagrida@gmail.com>
2025-09-15 01:16:28 +0200

    wip

---
+++ supabase/migrations/00000_create_helper_functions.sql
@@ -0,0 +1,10 @@
+-- Create helper functions that are used across multiple migrations
+
+-- Function to automatically update the updated_at timestamp
+CREATE OR REPLACE FUNCTION public.update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
